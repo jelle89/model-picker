173a72348116d296a9ac60e860c9be31c0870986
jelle89 <jelle.monen@hotmail.com>
2019-06-22 14:26:10 +0200

    finished step 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import store from './store'
+// import {Provider} from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const data = [
+  {
+    name: "Ivel Z3",
+    manufacturer: "Ivasim",
+    year: 1969,
+    origin: "Croatia"
+  },
+  {
+    name: "Bally Astrocade",
+    manufacturer: "Bally Consumer Products",
+    year: 1977,
+    origin: "USA"
+  },
+  {
+    name: "Sord M200 Smart Home Computer",
+    manufacturer: "Sord Computer Corporation",
+    year: 1971,
+    origin: "Japan"
+  },
+  {
+    name: "Commodore 64",
+    manufacturer: "Commodore",
+    year: 1982,
+    origin: "USA"
+  }
+]
 
-export default App;
+class App extends React.Component {
+ 
+  updateSelection= (event) => {
+    const eventValue = event.target.value
+    this.setState({event: event});
+    console.log(eventValue)
+  
+  }
+  
+    render() {
+      return (
+        <div className="model">
+  
+          <select onChange={this.updateSelection}>
+            <option value=''>--pick a model</option>
+            { Object.keys(data).map(model => 
+            <option value={model.name}>{'${data[model].name} (${data[model].year}) '}</option>)}
+          </select>
+        </div>
+      );
+    }
+  }
+  
+
+  export default App;
